7c846bbeabbdafc1cefa9e0e645849cbab8c2030
Luke Spang <lspang20@gmail.com>
2021-08-05 10:50:10 -0400

    index route

--- routes/html.js
+++ routes/html.js
@@ -1,2 +1,8 @@
 const express = require("express");
 const path = require("path");
+
+module.exports = function (app){
+    app.get("/", (req, rest)=>{
+        rest.sendFile(path.join(__dirname, "../public/index.html"));
+    })
+}
