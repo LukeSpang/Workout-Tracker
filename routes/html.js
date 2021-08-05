a2970c2dc68aa512e4aa3d5e87da046f59244404
Luke Spang <lspang20@gmail.com>
2021-08-05 10:52:12 -0400

    route for exercist html

--- routes/html.js
+++ routes/html.js
@@ -4,5 +4,11 @@ const path = require("path");
 module.exports = function (app){
     app.get("/", (req, rest)=>{
         rest.sendFile(path.join(__dirname, "../public/index.html"));
-    })
+    });
+
+    app.get("/exercise", (req, res)=>{
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
+
+   
 }
