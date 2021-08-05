b1a31b91d03c5cf75855ea25217f523a94a21b4c
Luke Spang <lspang20@gmail.com>
2021-08-05 10:00:50 -0400

    express set up

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const router = require("./Develop/routes/api")
 
+const PORT = process.env.PORT || 3333;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
