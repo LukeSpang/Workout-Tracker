d38d556c0d712163566487b4245d2510956d43fd
Luke Spang <lspang20@gmail.com>
2021-08-05 14:08:05 -0400

    working locally

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ mongoose.connect(
     }
 );
 
-app.use(require('./routes/api-routes.js'));
-app.use(require('./routes/html-routes.js')); 
+app.use(require('./routes/api.js'));
+app.use(require('./routes/html.js')); 
 
 app.listen(PORT, () => {
 console.log(`App running on port ${PORT}!`);
