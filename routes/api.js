33db7af469884620524273ef8b9e097617e65f59
Luke Spang <lspang20@gmail.com>
2021-08-05 10:47:13 -0400

    router get workouts and module export router

--- routes/api.js
+++ routes/api.js
@@ -39,3 +39,15 @@ router.get("/api/workouts/range", (req, res)=>{
         res.json(err);
     });
 });
+
+router.get("/api/workouts", (req, res)=>{
+    Workout.find({})
+    .then((dbWorkout)=>{
+        res.json(dbWorkout);
+    })
+    .catch((err)=>{
+        res.json(err);
+    });
+});
+
+module.exports = router;
