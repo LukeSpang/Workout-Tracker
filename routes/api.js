60b0c8fcbf70531848c74e9410cb3d08bcf6b3d9
Luke Spang <lspang20@gmail.com>
2021-08-05 10:39:16 -0400

    api workouts post route

--- routes/api.js
+++ routes/api.js
@@ -2,3 +2,13 @@ const Workout = require("../models/workout.js");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
+
+router.post("/api/workouts", ({body}, res)=>{
+    Workout.create({})
+    .then((dbWorkout)=>{
+        res.json(dbWorkout);
+    })
+    .catch(({message})=>{
+        console.log(message);
+    });
+});
