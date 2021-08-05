137923a95933543b1e28d9e641c154c90e583a0a
Luke Spang <lspang20@gmail.com>
2021-08-05 13:07:35 -0400

    workout.js issues

--- models/workout.js
+++ models/workout.js
@@ -1,39 +1,37 @@
 const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema(
-    {
-        day: { type: Date, default: () => new Date()},
-        exercise: [
-            {
-                type: {
-                    type: String,
-                },
-                name: {
-                    type: String,
-                },
-                duration: {
-                    type: Number,
-                },
-                weight: Number,
-                reps: Number,
-                sets: Number,
-                distance: Number,
-            },
-        ],
-    },
-    {
-        toJSON: {
-            virtuals: true,
-        },
-    }
-);
-
-WorkoutSchema.virtual("totalDuration").get(function (){
-    const duration = this.exercises.reduce((acc, cur)=>{
-        return acc + cur.duration;
-    }, 0);
-    return duration;
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: () => new Date()
+  },
+  exercises: [
+      {
+        type: {
+            type: String,
+          },
+          name: {
+            type: String,
+          },
+          duration: {
+            type: Number,
+          },
+          distance: {
+            type: Number,
+          },
+          weight: {
+            type: Number,
+          },
+          reps: {
+            type: Number,
+          },
+          sets: {
+            type: Number,
+          },
+      }
+  ],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
