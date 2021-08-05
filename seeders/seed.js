26caf25e1d96ce7ed6887373ee8cb090c5de3344
Luke Spang <lspang20@gmail.com>
2021-08-05 12:07:54 -0400

    fixed model title

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db = require('../models/workout.js');
+const db = require('../models/Workout.js');
 
 mongoose.connect('mongodb://localhost/workout', {
   useNewUrlParser: true,
--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require("express");
-// const logger = require("morgan");
 const mongoose = require("mongoose");
-// const router = require("./routes/api.js")
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-// app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
