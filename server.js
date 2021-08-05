7a62b89b8cd8074a11146440f7c918b3c236f0df
Luke Spang <lspang20@gmail.com>
2021-08-05 12:53:39 -0400

    heroku error, trying server

--- server.js
+++ server.js
@@ -11,21 +11,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/workout",{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    }
-);
-//////routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"))
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 
-app.listen(PORT, ()=>{
-    console.log(`listening on port ${PORT}`);
 });
 
+// routes
+app.use(require("./routes/html.js"));
+app.use(require("./routes/api.js"));
 
-
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
